801ea788f09dc03b43ee3d379bf58c65e149b2b2
dani campa <danicampa90@gmail.com>
2017-12-29 21:56:23 +0100

    add day12. Fix test compile error in day10

--- compileAll.sh
+++ compileAll.sh
@@ -5,3 +5,11 @@
 (cd day4 && cargo test && cd ..) || exit 1
 (cd day5 && cargo test && cd ..) || exit 1
 (cd day6 && cargo test && cd ..) || exit 1
+(cd day7 && cargo test && cd ..) || exit 1
+(cd day8 && cargo test && cd ..) || exit 1
+(cd day9 && cargo test && cd ..) || exit 1
+(cd day10 && cargo test && cd ..) || exit 1
+(cd day11 && cargo test && cd ..) || exit 1
+#(cd day12 && cargo test && cd ..) || exit 1
+#(cd day13 && cargo test && cd ..) || exit 1
+#(cd day14 && cargo test && cd ..) || exit 1
