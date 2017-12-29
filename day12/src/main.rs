4b48d66040558cde4761414e9a5b1b9cdcc38f8c
dani campa <danicampa90@gmail.com>
2017-12-29 22:50:29 +0100

    Day 12: add input file and data reading routines.

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,5 +1,59 @@
+extern crate libutils;
+use libutils::read_file_foreach_line;
+use std::error::Error;
+
+// map from index to neighbors list.
+type Graph = Vec<Vec<usize>>;
+
+#[derive(Debug)]
+enum ParsingError {
+    PrematureEndOfLine,
+    NumberParseError,
+}
+fn remove_ending_comma(word: &str) -> &str {
+    if word.ends_with(',') {
+        &word[0..word.len()-1]
+    } else {
+        word
+    }
+}
+
+fn load_line(line: &str, data: &mut Graph) -> Result<(), ParsingError>{
+    println!("{}",line );
+    let mut line_splitted = line.split_whitespace();
+    // gather index and skip the "<-->"
+    let index : usize =  line_splitted
+        .next()
+        .ok_or(ParsingError::PrematureEndOfLine)?
+        .parse().map_err(|_| ParsingError::NumberParseError)?;
+    line_splitted
+        .next()
+        .ok_or(ParsingError::PrematureEndOfLine)?; // skip "<-->"
+    
+    // parse neighbors list
+    let mut neighbors = vec!();
+    for neighbor in line_splitted {
+        let value = remove_ending_comma(neighbor)
+            .parse()
+            .map_err(|_| ParsingError::NumberParseError)?;
+        neighbors.push(value);
+    }
+    data[index] = neighbors;
+    Ok(())
+}
 
 
 fn main() {
+    let fname = "input.txt";
+    let mut graph : Graph = vec!();
+    graph.resize(2000,vec!());
+    read_file_foreach_line(fname, &mut |line| { load_line(line.as_ref(), &mut graph) } ).unwrap();
+    for (neighbors, index) in graph.into_iter().zip(0..) {
+        print!("Index {} has these neighbors ", index);
+        for n in neighbors {
+            print!("{},", n);
+        }
+        println!();
+    }
 
 }
