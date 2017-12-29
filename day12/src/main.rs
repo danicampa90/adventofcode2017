8adc53f47eb68770eaf10c9f617d8854687b2afa
dani campa <danicampa90@gmail.com>
2017-12-29 23:03:16 +0100

    Day 12: first star

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,6 +1,6 @@
 extern crate libutils;
 use libutils::read_file_foreach_line;
-use std::error::Error;
+use std::collections::HashSet;
 
 // map from index to neighbors list.
 type Graph = Vec<Vec<usize>>;
@@ -21,6 +21,7 @@ fn remove_ending_comma(word: &str) -> &str {
 fn load_line(line: &str, data: &mut Graph) -> Result<(), ParsingError>{
     println!("{}",line );
     let mut line_splitted = line.split_whitespace();
+
     // gather index and skip the "<-->"
     let index : usize =  line_splitted
         .next()
@@ -38,22 +39,41 @@ fn load_line(line: &str, data: &mut Graph) -> Result<(), ParsingError>{
             .map_err(|_| ParsingError::NumberParseError)?;
         neighbors.push(value);
     }
+
     data[index] = neighbors;
     Ok(())
+}
+
+fn reachability(graph:&Graph) -> Vec<usize> {
+    let mut visited : HashSet<usize> = HashSet::new();
+    let mut queue : HashSet<usize> = HashSet::new();
+    queue.insert(0);
+    while !queue.is_empty() {
+        let item : usize = *queue.iter().next().unwrap();
+        queue.remove(&item);
+        if visited.insert(item) {
+            queue.extend(&graph[item])
+        }
+    }
+    return visited.iter().map(|item| *item).collect();
 }
 
 
 fn main() {
     let fname = "input.txt";
     let mut graph : Graph = vec!();
-    graph.resize(2000,vec!());
+    graph.resize(2000, vec!());
     read_file_foreach_line(fname, &mut |line| { load_line(line.as_ref(), &mut graph) } ).unwrap();
-    for (neighbors, index) in graph.into_iter().zip(0..) {
+    for (neighbors, index) in (&graph).into_iter().zip(0..) {
         print!("Index {} has these neighbors ", index);
         for n in neighbors {
             print!("{},", n);
         }
         println!();
     }
+
+    let count = reachability(&graph).len();
+    println!("Reachable states: {}",count);
+
 
 }
