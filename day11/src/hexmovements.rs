ff12405c9e676c75206e25f8bcd81bb5fca511fe
dani campa <danicampa90@gmail.com>
2017-12-29 21:17:17 +0100

    Day 11: First star

---
+++ day11/src/hexmovements.rs
@@ -0,0 +1,138 @@
+use std;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Movement {
+    North,
+    South,
+    SouthEast,
+    NorthEast,
+    SouthWest,
+    NorthWest,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Position { 
+    x: i32,
+    y: i32
+}
+
+impl Position{
+    pub fn origin() -> Position {
+        Position::new(0,0)
+    }
+
+    pub fn new(x: i32, y:i32) -> Position {
+        Position{x:x, y:y}
+    }
+
+    pub fn dist_squared(x: i32, y:i32) -> i32 {
+        return x*x + y*y;
+    }
+}
+
+fn move_hex(p:Position, m: Movement) -> Position {
+    let odd_row_offset = if p.x % 2 == 0 { 0 } else { -1 };
+    match m {
+        Movement::North =>     Position{x: p.x, y: p.y + 1},
+        Movement::South =>     Position{x: p.x, y: p.y - 1},
+        // next ones depend if we are on odd or even number in x axis
+        Movement::NorthEast => Position{x: p.x+1, y: p.y + 1 + odd_row_offset},
+        Movement::SouthEast => Position{x: p.x+1, y: p.y + odd_row_offset},
+        Movement::NorthWest => Position{x: p.x-1, y: p.y + 1 + odd_row_offset},
+        Movement::SouthWest => Position{x: p.x-1, y: p.y + odd_row_offset},
+    }
+}
+
+pub fn walk_hex(start: Position, directions: &[Movement]) -> Position {
+    let mut pos = start;
+    for dir in directions {
+        pos = move_hex(pos, *dir);
+    }
+    pos
+}
+
+
+pub fn get_path(mut pos: Position) -> i32 {
+    let mut total_count = 0;
+    while pos != Position::origin() {
+        let decision = decide_movement(pos);
+        if decision.is_none() {
+            break;
+        }
+        let (movement, count) = decision.unwrap();
+        println!("From {:?} move {:?} {} times", pos, movement, count);
+        total_count += count;
+        for _repetiton in 0..count {
+            pos = move_hex(pos, movement);
+        }
+    }
+    return total_count;
+}
+
+pub fn decide_movement(pos: Position) -> Option<(Movement, i32)> {
+    /*! Returns direction and number of steps in that direction that gets closer to the objective
+     * Target is the origin (0,0). 
+     **/
+    if pos.x == 0 { 
+        if pos.y == 0 {
+            return None;
+        }
+        // easy case: move up or down directly.
+        if pos.y > 0 {
+            return Some((Movement::South,  pos.y));
+        } else {
+            return Some((Movement::North, -pos.y));
+        }
+    }
+    if pos.y == 0 {
+        if pos.x < 0 {
+            return Some((Movement::NorthEast, 1))
+        } else {
+            return Some((Movement::NorthWest, 1))
+        }
+    }
+
+    
+    if pos.x > 0 && pos.y > 0 {
+        let distance = std::cmp::min(pos.x, pos.y);
+        return Some((Movement::SouthWest, distance))
+    } else if pos.x < 0 && pos.y > 0 {
+        let distance = std::cmp::min(-pos.x, pos.y);
+        return Some((Movement::SouthEast, distance))
+    } else if pos.x > 0 && pos.y < 0 {
+        let distance = std::cmp::min(pos.x, -pos.y);
+        return Some((Movement::NorthWest, distance))
+    } else if pos.x < 0 && pos.y < 0 {
+        let distance = std::cmp::min(-pos.x, -pos.y);
+        return Some((Movement::NorthEast, distance))
+    } else { unreachable!() };
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::Movement::*;
+    use super::walk_hex;
+    use super::Position;
+
+    #[test]
+    fn test_movement_around() {
+        let origin = Position{ x: 0, y:0 };
+        let walk = [North, NorthEast, SouthEast, South, NorthWest, SouthWest];
+        assert_eq!(walk_hex(origin, &walk), origin);
+    }
+    #[test] 
+    fn test_movement_neneswsw() {
+        let origin = Position{ x: 0, y:0 };
+        let walk = [NorthEast, NorthEast, SouthWest, SouthWest];
+        assert_eq!(walk_hex(origin, &walk), origin);
+    }
+    #[test] 
+    fn test_movement_neness() {
+        let origin = Position{ x: 0, y:0 };
+        let walk = [NorthEast, NorthEast, South, South];
+        assert_eq!(walk_hex(origin, &walk), Position{ x: 2, y:-1 });
+    }
+
+}
---
+++ day11/src/main.rs
@@ -0,0 +1,40 @@
+extern crate libutils;
+use libutils::read_file_to_str;
+
+mod hexmovements;
+
+use hexmovements::walk_hex;
+use hexmovements::get_path;
+use hexmovements::Position;
+use hexmovements::Movement;
+
+
+fn parse_direction(mov_direction: &str) -> Result<Movement,()> {
+    match mov_direction {
+        "n" => Ok(Movement::North),
+        "s" => Ok(Movement::South),
+        "ne" => Ok(Movement::NorthEast),
+        "se" => Ok(Movement::SouthEast),
+        "nw" => Ok(Movement::NorthWest),
+        "sw" => Ok(Movement::SouthWest),
+        _ => Err(())
+    }
+}
+
+fn parse_directions(line: &str) -> Vec<Movement> {
+    let mut movs = Vec::new();
+    for split in line.split(",") {
+        movs.push(parse_direction(split).unwrap());
+    }
+    let end_pos = walk_hex(Position::origin(), movs.as_slice());
+    println!("Final position: {:?}", end_pos);
+    let distance = get_path(end_pos);
+    println!("Distance: {}", distance);
+    return movs;
+
+}
+
+fn main() {
+    let content = read_file_to_str("input.txt").unwrap();
+    parse_directions(content.as_str());
+}
