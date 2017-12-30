e16cb810b2baad0071acecea650087e5a2078882
dani campa <danicampa90@gmail.com>
2017-12-30 11:53:33 +0100

    Day3: Add solution for second star

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,5 @@
 // Part 2 is incomplete!
-
+use std::collections::HashMap;
 
 fn manhattan(x: i32, y:i32) -> i32 {
     return if x<0 {-x} else {x} + if y<0 {-y} else {y} 
@@ -8,7 +8,10 @@ fn find_distance(target: i32) -> i32 {
     let (x,y) = find_coords(target);
     manhattan(x,y)
 }
-
+/// Given an index i it returns the coordinates of the i-th cell in the spiral.
+/// It's not fully optimized (especially for the while/for loops that go around the spiral).
+/// but it goes "out" quite quickly because it exploits the numeric series of the rings.
+/// Note: doesn't work with target = 0
 fn find_coords(target: i32) -> (i32,i32) {
     let target = target - 1; // we start from 0-based indexes
 
@@ -77,10 +80,50 @@ fn find_coords(target: i32) -> (i32,i32) {
 
 }
 
+// ///////////////// Second star section ///////////////////
+// Following functions are the implementation of the second star problem.
+// It also uses the function above (find_coords) to go around the spiral
 
+/// Type of the map
+type Map = HashMap<(i32,i32), i32>;
+
+/// Sums the neighbors on the map around the specified point
+fn sum_neighbors(map: &Map, x:i32, y: i32) -> i32 {
+    let mut sum = 0;
+    for x_offset in -1..2 { // 2 excluded: [-1, 0, 1]
+        for y_offset in -1..2 { // 2 excluded: [-1, 0, 1]
+            sum += map.get(&(x_offset+x, y_offset+y)).unwrap_or(&0);
+        }
+    }
+    return sum;
+}
+
+/// Solves the second star problem
+/// Note that it's very inefficient at the moment. It calls every
+/// iteration the function find_coords to find the next coord to fill.
+/// This could be optimized by going around the spiral in an iterative way.
+fn star2(input: i32) -> i32{
+    let mut map: Map = HashMap::new();
+    map.insert((0,0), 1);
+    let mut index = 1;
+    loop {
+        let (x,y) = find_coords(index);
+        let sum = sum_neighbors(&map, x,y);
+        //println!("Insert {} in {},{}", sum, x, y);
+        map.insert((x,y), sum);
+        if sum > input {
+            return sum;
+        }
+        index += 1;
+    }
+}
+
+/// Main
 fn main() {
     let distance = find_distance(347991);
-    println!("{}", distance);
+    println!("Distance: {}", distance);
+    let second_star = star2(347991);
+    println!("Next value larger than 347991: {}", second_star );
 }
 
 #[cfg(test)]
